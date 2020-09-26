57df24e5d760a6a02a9ffcbd676069a18679dd6d
Andrii <riabchun.andrii@gmail.com>
2020-09-26 10:30:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,27 @@
 import telebot
-bot = telebot.TeleBot("1381051273:AAHATq_xhk-piS6dU2Txv2P6POGor5pNoOs")
 import datetime
 import time  
 timing = time.time()-45000 
-pollnumber=0
-
-date_today=datetime.date.today() + datetime.timedelta(days=0) 
-date_tomorrow=datetime.date.today() + datetime.timedelta(days=1) 
-date_dayafter=datetime.date.today() + datetime.timedelta(days=2) 
-question_today = "Волейбол "+str(date_today.day)+"."+str(date_today.strftime("%m"))+" ("+date_today.strftime("%a")+")?"  
-question_tomorrow = "Волейбол "+str(date_tomorrow.day)+"."+str(date_tomorrow.strftime("%m"))+" ("+date_tomorrow.strftime("%a")+")?" 
-question_dayafter = "Волейбол "+str(date_dayafter.day)+"."+str(date_dayafter.strftime("%m"))+" ("+date_dayafter.strftime("%a")+")?" 
+bot = telebot.TeleBot("1381051273:AAHATq_xhk-piS6dU2Txv2P6POGor5pNoOs")
 options = ["Да", "50/50", "Нет"]
 
-@bot.message_handler(commands=['dellpoll']) 
-def send_welcome(message): 
-        MEMBER_ID = message.from_user.id  
-        GROUP_ID = message.chat.id 
-        MEMBER_RANK = bot.get_chat_member(GROUP_ID, MEMBER_ID).status
-        global pollnumber 
-        if MEMBER_RANK=="creator": 
-                if pollnumber!=0: 
-                        bot.delete_message(GROUP_ID, pollnumber) 
-                        pollnumber=0
-        bot.delete_message(GROUP_ID, message.message_id) 
 
-@bot.message_handler(commands=['delltime'])
+def rtoday(): 
+        date_today=datetime.date.today() + datetime.timedelta(days=0) 
+        global question_today
+        question_today = "Волейбол сегодня "+str(date_today.day)+"."+str(date_today.strftime("%m"))+" ("+date_today.strftime("%a")+")?"
+
+def rtomorrow():
+        date_tomorrow=datetime.date.today() + datetime.timedelta(days=1) 
+        global question_tomorrow
+        question_tomorrow = "Волейбол завтра "+str(date_tomorrow.day)+"."+str(date_tomorrow.strftime("%m"))+" ("+date_tomorrow.strftime("%a")+")?"
+
+def rdayafter(): 
+        date_dayafter=datetime.date.today() + datetime.timedelta(days=2) 
+        global question_dayafter
+        question_dayafter = "Волейбол послезавтра "+str(date_dayafter.day)+"."+str(date_dayafter.strftime("%m"))+" ("+date_dayafter.strftime("%a")+")?" 
+
+@bot.message_handler(commands=['cooldown'])
 def send_welcome(message):
         MEMBER_ID = message.from_user.id
         GROUP_ID = message.chat.id
@@ -37,13 +33,14 @@ def send_welcome(message):
                         bot.send_message(GROUP_ID, 'Куллдаун сброшен')
         bot.delete_message(GROUP_ID, message.message_id)    
 
-@bot.message_handler(commands=['volley_today'])
-def send_welcome(message):
+@bot.message_handler(commands=['today'])
+def volley_today(message):
         MEMBER_ID = message.from_user.id
         GROUP_ID = message.chat.id
         MEMBER_RANK = bot.get_chat_member(GROUP_ID, MEMBER_ID).status
         global timing
         global pollnumber
+        rtoday()
         if time.time() - timing > 43200:  
                 if MEMBER_RANK=="creator" or MEMBER_RANK=="administrator":
                         bot.send_poll(chat_id=GROUP_ID, question=question_today+' - '+bot.get_chat_member(GROUP_ID, MEMBER_ID).user.first_name, options = options, is_anonymous=False) 
@@ -52,13 +49,14 @@ def send_welcome(message):
                         pollnumber = message.message_id+1 
         bot.delete_message(GROUP_ID, message.message_id)  
 
-@bot.message_handler(commands=['volley_tomorrow'])
-def send_welcome(message):
+@bot.message_handler(commands=['tomorrow'])
+def volley_tomorrow(message):
         MEMBER_ID = message.from_user.id
         GROUP_ID = message.chat.id
         MEMBER_RANK = bot.get_chat_member(GROUP_ID, MEMBER_ID).status
         global timing
         global pollnumber
+        rtomorrow()
         if time.time() - timing > 43200:
                 if MEMBER_RANK=="creator" or MEMBER_RANK=="administrator":
                         bot.send_poll(chat_id=GROUP_ID, question=question_tomorrow+' - '+bot.get_chat_member(GROUP_ID, MEMBER_ID).user.first_name, options = options, is_anonymous=False)
@@ -67,13 +65,14 @@ def send_welcome(message):
                         pollnumber = message.message_id+1
         bot.delete_message(GROUP_ID, message.message_id)  
 
-@bot.message_handler(commands=['volley_dayafter'])
-def send_welcome(message):
+@bot.message_handler(commands=['dayafter'])
+def volley_dayafter(message):
         MEMBER_ID = message.from_user.id
         GROUP_ID = message.chat.id
         MEMBER_RANK = bot.get_chat_member(GROUP_ID, MEMBER_ID).status
         global timing
         global pollnumber
+        rdayafter()
         if time.time() - timing > 43200:
                 if MEMBER_RANK=="creator" or MEMBER_RANK=="administrator":
                         bot.send_poll(chat_id=GROUP_ID, question=question_dayafter+' - '+bot.get_chat_member(GROUP_ID, MEMBER_ID).user.first_name, options = options, is_anonymous=False)
