79cc2dffc7846cb254bca527233c1d8f473dcc12
Andrii <riabchun.andrii@gmail.com>
2020-09-28 13:53:51 +0300

    1

--- main.py
+++ main.py
@@ -2,9 +2,38 @@ import telebot
 from telebot import types
 import datetime
 import time  
+from pyowm import OWM
+from pyowm.utils import timestamps
+from pyowm.utils import config as cfg
 timing = time.time()-45000 
 bot = telebot.TeleBot("1381051273:AAHATq_xhk-piS6dU2Txv2P6POGor5pNoOs")
 options = ["Могу", "50/50", "Не смогу"]
+
+config = cfg.get_default_config()
+config['language'] = 'ru'
+owm = OWM('c30a0f10eeb9a93501989842cfd6bb23', config)
+cooldown = time.time()-120
+
+@bot.message_handler(commands=['weather'])
+def weather(message):
+        global cooldown
+        GROUP_ID = message.chat.id
+        mgr = owm.weather_manager()
+        place = mgr.weather_at_place('Kyiv')
+        wt = place.weather
+        h_forecaster = mgr.forecast_at_place('Kyiv', '3h')
+        tomorrow_at_five = timestamps.tomorrow(17, 0)     
+        wtm = h_forecaster.get_weather_at(tomorrow_at_five)  
+        if time.time() - cooldown > 120:  
+                bot.send_message(GROUP_ID, 'Сегодня '+wt.detailed_status+'.\n'+str(round(wt.temperature('celsius')['temp']))+'°С 🌡\n\n'+'Завтра '+wtm.detailed_status+'.\n'+str(round(wtm.temperature('celsius')['temp']))+'°С 🌡\n\n')
+                cooldown = time.time()
+        else:
+                bot.delete_message(GROUP_ID, message.message_id)   
+        
+
+
+
+
 
 @bot.message_handler(commands=['sendpoll'])
 def start(message):
@@ -25,7 +54,8 @@ def start(message):
                 kid = message.message_id+1
                 bot.send_message(chat_id=message.chat.id, text='Когда пойдем на волейбол?', reply_markup=markup)
                 timing = time.time()
-        bot.delete_message(GROUP_ID, message.message_id)   
+        else:
+                bot.delete_message(GROUP_ID, message.message_id)      
 
 @bot.callback_query_handler(func=lambda call: True)
 def query_handler(call):
@@ -47,11 +77,17 @@ def volley_to1():
         global timing
         rtoday()
         if MEMBER_RANK=="creator" or MEMBER_RANK=="administrator":
-                bot.send_poll(chat_id=GROUP_ID, question=question_today+' - '+bot.get_chat_member(GROUP_ID, MEMBER_ID).user.first_name, options = options, is_anonymous=False) 
+                bot.send_poll(chat_id=GROUP_ID, question=question_today, options = options, is_anonymous=False) 
+                bot.send_message(GROUP_ID, 'Сегодня '+wt.detailed_status+'.\n'+str(round(wt.temperature('celsius')['temp']))+'°С 🌡')
+                
 def rtoday(): 
         date_today=datetime.date.today() + datetime.timedelta(days=0) 
         global question_today
+        global wt
         question_today = "Волейбол сегодня "+str(date_today.day)+"."+str(date_today.strftime("%m"))+" ("+date_today.strftime("%A")+")?"
+        mgr = owm.weather_manager()
+        place = mgr.weather_at_place('Kyiv')
+        wt = place.weather
 
 # ----- #     
                 
@@ -59,11 +95,18 @@ def volley_to2():
         global timing
         rtomorrow()
         if MEMBER_RANK=="creator" or MEMBER_RANK=="administrator":
-                bot.send_poll(chat_id=GROUP_ID, question=question_tomorrow+' - '+bot.get_chat_member(GROUP_ID, MEMBER_ID).user.first_name, options = options, is_anonymous=False)
+                bot.send_poll(chat_id=GROUP_ID, question=question_tomorrow, options = options, is_anonymous=False)
+                bot.send_message(GROUP_ID, 'Завтра '+wtm.detailed_status+'.\n'+str(round(wtm.temperature('celsius')['temp']))+'°С 🌡')
 def rtomorrow():
         date_tomorrow=datetime.date.today() + datetime.timedelta(days=1) 
         global question_tomorrow
+        global wtm
         question_tomorrow = "Волейбол завтра "+str(date_tomorrow.day)+"."+str(date_tomorrow.strftime("%m"))+" ("+date_tomorrow.strftime("%A")+")?"
+        mgr = owm.weather_manager()
+        h_forecaster = mgr.forecast_at_place('Kyiv', '3h')
+        tomorrow_at_five = timestamps.tomorrow(17, 0)     
+        wtm = h_forecaster.get_weather_at(tomorrow_at_five) 
+        
 
 # ----- #  
 
@@ -71,7 +114,7 @@ def volley_to3():
         global timing
         rdayafter()
         if MEMBER_RANK=="creator" or MEMBER_RANK=="administrator":
-                bot.send_poll(chat_id=GROUP_ID, question=question_dayafter+' - '+bot.get_chat_member(GROUP_ID, MEMBER_ID).user.first_name, options = options, is_anonymous=False)
+                bot.send_poll(chat_id=GROUP_ID, question=question_dayafter, options = options, is_anonymous=False)
 def rdayafter(): 
         date_dayafter=datetime.date.today() + datetime.timedelta(days=2) 
         global question_dayafter
@@ -84,12 +127,15 @@ def send_welcome(message):
         MEMBER_ID = message.from_user.id
         GROUP_ID = message.chat.id
         MEMBER_RANK = bot.get_chat_member(GROUP_ID, MEMBER_ID).status
-        if MEMBER_RANK=="creator":
-                global timing 
-                if timing>600: 
+        global timing 
+        global cooldown
+        if timing>600: 
+                if MEMBER_RANK=="creator":
                         timing=0 
-                        bot.send_message(GROUP_ID, 'Куллдаун сброшен')
-        bot.delete_message(GROUP_ID, message.message_id)    
+                        cooldown=0
+                        bot.send_message(GROUP_ID, 'Куллдауны сброшен')
+        else:
+                bot.delete_message(GROUP_ID, message.message_id)     
 
 
 @bot.message_handler(content_types=["new_chat_members"])
@@ -97,5 +143,6 @@ def handler_new_member(message):
     user_name = message.new_chat_members[0].first_name
     bot.send_message(message.chat.id, "Добро пожаловать, {0}!".format(user_name))   
         
- 
+
+
 bot.polling() 
