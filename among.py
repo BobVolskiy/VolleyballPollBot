cd195b7207b4e2fb1137be9bc40b347badb10711
Andrii <riabchun.andrii@gmail.com>
2020-10-02 02:07:43 +0300

    Update among.py

--- among.py
+++ among.py
@@ -12,7 +12,15 @@ def amongus(message):
                 bot.send_sticker(GROUP_ID,'CAACAgEAAxkBAAEBZrxfdlrV2gUdNBh_sdOQ9nqOK9wlogACRgADnjOcH9odHIXtfgmvGwQ')
                 bot.send_message(GROUP_ID, name+' призывает всех поиграть в Among Us!\n\nЗаходи в голосовой канал, ссылка на игру будет в чате --> https://discord.gg/Z3rKddR')
                 cooldown=time.time()
-
+@bot.message_handler(commands=['cooldown'])
+def send_welcome(message):
+        MEMBER_ID = message.from_user.id
+        GROUP_ID = message.chat.id
+        MEMBER_RANK = bot.get_chat_member(GROUP_ID, MEMBER_ID).status
+        global cooldown
+        if MEMBER_RANK=="Administrator" or message.from_user.first_name=="Боб":
+                 cooldown=0
+                 bot.send_message(GROUP_ID, 'Куллдаун сброшен')
       
 
 bot.polling() 
